901d7afcda6cd81204ad7d2a922594e6e6eda03e
Cotes <cotes.chung@gmail.com>
2019-02-03 01:04:37 +0800

    Moved JS fils.

--- assets/js/common.js
+++
@@ -1,9 +0,0 @@
----
----
-
-{% include_relative dist/commons/back-to-top.min.js %}
-{% include_relative dist/commons/search-display.min.js %}
-{% include_relative dist/commons/sidebar.min.js %}
-{% include_relative dist/commons/topbar-switch.min.js %}
-{% include_relative dist/commons/topbar-title.min.js %}
-{% include_relative dist/commons/syntax.min.js %}
---
+++ assets/js/dist/commons.js
@@ -0,0 +1,9 @@
+---
+---
+
+{% include_relative commons/back-to-top.min.js %}
+{% include_relative commons/search-display.min.js %}
+{% include_relative commons/sidebar.min.js %}
+{% include_relative commons/topbar-switch.min.js %}
+{% include_relative commons/topbar-title.min.js %}
+{% include_relative commons/syntax.min.js %}

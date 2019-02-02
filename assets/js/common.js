0425882c66310c424d6174a840027de0f43fa2fb
Cotes <cotes.chung@gmail.com>
2019-02-03 00:45:51 +0800

    Removed the duplicated output  of JS files.

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,10 +1,9 @@
 ---
 ---
 
-{% include_relative dist/back-to-top.min.js %}
-{% include_relative dist/category-collapse.min.js %}
-{% include_relative dist/search-display.min.js %}
-{% include_relative dist/sidebar.min.js %}
-{% include_relative dist/topbar-switch.min.js %}
-{% include_relative dist/topbar-title.min.js %}
-{% include_relative dist/syntax.min.js %}
+{% include_relative dist/commons/back-to-top.min.js %}
+{% include_relative dist/commons/search-display.min.js %}
+{% include_relative dist/commons/sidebar.min.js %}
+{% include_relative dist/commons/topbar-switch.min.js %}
+{% include_relative dist/commons/topbar-title.min.js %}
+{% include_relative dist/commons/syntax.min.js %}

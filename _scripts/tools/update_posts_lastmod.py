85ba76a0700a135adb5ae3d1819a4296ee09d74f
Cotes Chung <cotes.chung@gmail.com>
2019-09-18 16:12:30 +0800

    Added initial script.

--- _scripts/tools/update_posts_lastmod.py
+++ _scripts/tools/update_posts_lastmod.py
@@ -25,7 +25,7 @@ from ruamel.yaml import YAML
 POSTS_PATH = "_posts"
 
 
-def update_lastmod(verbose, commit):
+def update_lastmod(verbose):
     count = 0
     yaml = YAML()
 
@@ -84,12 +84,6 @@ def update_lastmod(verbose, commit):
 
     print ("[INFO] Success to update lastmod for {} post(s).").format(count)
 
-    if commit:
-        if count > 0:
-            subprocess.call(["git", "add", POSTS_PATH])
-            subprocess.call(["git", "commit", "-m",
-                            "[Automation] Updated lastmod for post(s)."])
-
 
 def help():
     print("Usage: "
@@ -100,7 +94,6 @@ def help():
 
 def main():
     verbose = False
-    commit = False
 
     if len(sys.argv) > 1:
         for arg in sys.argv:
@@ -109,14 +102,12 @@ def main():
             else:
                 if arg == '-v' or arg == '--verbose':
                     verbose = True
-                elif arg == '-c' or arg == '--commit':
-                    commit = True
                 else:
                     print("Oops! Unknown argument: '{}'\n".format(arg))
                     help()
                     return
 
-    update_lastmod(verbose, commit)
+    update_lastmod(verbose)
 
 
 main()
